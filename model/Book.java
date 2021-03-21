f2a2253094fc1a1c026d3e7debeacfe7cab388aa
tofikhidayatxyz <topikhidayat711@gmail.com>
2021-03-21 12:22:13 +0700

    change: main

--- model/Book.java
+++ model/Book.java
@@ -1,11 +1,12 @@
-package property;
+package model;
 
 public class Book {
-
+    public int id;
     public String code;
     public String title;
 
-    public Book(String code, String title) {
+    public Book(int id, String title, String code) {
+        this.id = id;
         this.code = code;
         this.title = title;
     }
--- model/Transaction.java
+++ model/Transaction.java
@@ -1,16 +1,16 @@
-package property;
+package model;
+
 
 public class Transaction {
-
+    public int id;
     public Student student;
     public Book book;
     public int amount;
-
-    public Transaction(Student student, Book book, int amount) {
+    
+    public Transaction(int id, Student student, Book book, int amount) {
+        this.id = id;
         this.student = student;
         this.book = book;
         this.amount = amount;
     }
-
 }
-
